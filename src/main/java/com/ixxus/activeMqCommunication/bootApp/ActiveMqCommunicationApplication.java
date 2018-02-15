11a987c15e2b17be685b096e31f5b380b72ca163
luismiguelcruz <luismiguelcruz@gmail.com>
2018-02-15 09:19:27 +0000

    added spring config

--- src/main/java/com/ixxus/activeMqCommunication/bootApp/ActiveMqCommunicationApplication.java
+++ src/main/java/com/ixxus/activeMqCommunication/bootApp/ActiveMqCommunicationApplication.java
@@ -1,12 +1,33 @@
 package com.ixxus.activeMqCommunication.bootApp;
 
+import com.ixxus.activeMqCommunication.jms.publisher.Producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
+import javax.jms.JMSException;
+
+//@SpringBootApplication
 public class ActiveMqCommunicationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ActiveMqCommunicationApplication.class, args);
+
+		//SpringApplication.run(ActiveMqCommunicationApplication.class, args);
+
+		final ApplicationContext context =
+				new ClassPathXmlApplicationContext("com/ixxus/activeMqCommunication/beans/spring-config.xml");
+
+		Producer producer = new Producer();
+
+		try {
+			producer.create("producer1", "queue1");
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println();
+
+		((ClassPathXmlApplicationContext)context).close();
 	}
 }
